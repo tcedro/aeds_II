52c12e77528f1fc2a8d196cc0abc60c8481f8f4d
tcedro <thiago.cedro@sga.pucminas.brgit config --global user.name tcedrogit config --global user.email thiago.cedro@sga.pucminas.br>
2023-05-13 14:35:19 -0300

    tp_04_fist_commit

---
+++ tp-04/binary-tree-java/Main.java
@@ -0,0 +1,486 @@
+//--------------------------arvore binaria----------------------------------//
+
+//class arvore binaria - pesquisa
+
+class BinaryTree 
+{
+    public NodePers root;
+    public int height;
+    
+    public BinaryTree() {
+        this.root = null;
+        this.height = 0;
+    }
+    
+    public BinaryTree(NodePers root) {
+        this.root = root;
+        this.height++;
+    }
+
+    //inserir
+    public void insert(Personagem pers) throws Exception{
+        root = insert(pers, root);
+    }
+    
+    //inserir recursivo
+    public NodePers insert(Personagem pers, NodePers i) throws Exception {
+        if(i == null) { i = new NodePers(pers); }
+        else if( pers.compare(i.getElement()) < 0 ) {
+            i.left = insert(pers, i.left);
+        } 
+        else if( pers.compare(i.getElement()) > 0 ) {
+            i.right = insert(pers, i.right);
+        } else { throw new Exception("ERROR."); }
+        
+        return i;
+    }
+
+    //pesquisar
+    public boolean search(String name) {
+        MyIO.print("raiz ");
+        return search(name, root);
+    }
+
+    //persquisar recursivo
+    public boolean search(String name, NodePers i) {
+        boolean ans = false;
+
+        if(i == null) ans = false; 
+        else if( name.compareTo(i.getElement().getNome() ) == 0 ) ans = true;
+        else if(name.compareTo(i.element.getNome()) < 0) { 
+            MyIO.print("esq ");
+            ans = search(name, i.left);
+        }
+        else {
+            MyIO.print("dir ");
+            ans = search(name, i.right);
+        }     
+        
+        // if(i == null) { ans = false; }
+        // else {
+        //     ans = ( ans ||  search(name, i.left) || search(name, i.right) );
+        // }
+        
+        return ans;
+    }
+
+    //getters setters
+    public NodePers getRoot() {
+        return root;
+    }
+    public void setRoot(NodePers root) {
+        this.root = root;
+    }
+    public int getHeight() {
+        return height;
+    }
+    
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+}
+
+//----------------------------Celula Personagem------------------------------//
+
+class NodePers 
+{
+    public Personagem element;
+    public NodePers left;
+    public NodePers right;
+
+    NodePers(Personagem pers) {
+        this(pers, null, null);
+    }
+    
+    NodePers(Personagem pers, NodePers left, NodePers right) {
+        this.element = pers;
+        this.left = left;
+        this.right = right;
+    }
+
+    public void setElement(Personagem element) {
+        this.element = element;
+    }
+
+    public boolean hasLeft(){
+        return this.left != null;
+    }
+
+    public boolean hasRight(){
+        return this.right != null;
+    }
+
+    public Personagem getElement() {
+        return element;
+    }
+
+    public NodePers getLeft() {
+        return left;
+    }
+
+    public NodePers getRight() {
+        return right;
+    }
+
+    public void setLeft(NodePers left) {
+        this.left = left;
+    }
+
+    public void setRight(NodePers right) {
+        this.right = right;
+    }
+}
+
+//-------------------------------Personagem ----------------------------------//
+
+class Personagem 
+{
+    //----------------------------atributes----------------------------------//
+
+    private String nome;
+    private int    altura;
+    private double peso;
+    private String corDoCabelo;
+    private String CorDaPele;
+    private String corDosOlhos;
+    private String anoNascimento;
+    private String genero;
+    private String homeworld;
+
+    //----------------------------constructors-------------------------------//
+
+    /*
+        Default
+    */
+    Personagem() {
+        this.nome = ""; 
+        this.altura = 0;
+        this.peso = 0.0; 
+        this.corDoCabelo = "";
+        this.CorDaPele = "";
+        this.corDosOlhos = "";
+        this.anoNascimento = "";
+        this.genero = "";
+        this.homeworld = "";
+    }
+    
+    /*
+        Parameters
+    */
+    Personagem(String nome, int altura, double peso, String corDoCabelo, String corDaPele,
+              String corDosOlhos, String anoNascimento, String genero, String homeworld) {
+        this.nome = nome; 
+        this.altura = altura;
+        this.peso = peso; 
+        this.corDoCabelo = corDoCabelo;
+        this.CorDaPele = corDaPele;
+        this.corDosOlhos = corDosOlhos;
+        this.anoNascimento = anoNascimento;
+        this.genero = genero;
+        this.homeworld = homeworld;
+    }
+
+    //----------------------------getters----------------------------------//
+
+    public String getNome()          { return this.nome; }
+    public int    getAltura()        { return this.altura; }
+    public double getPeso()          { return this.peso; }
+    public String getCorDoCabelo()   { return this.corDoCabelo; }
+    public String getCorDaPele()     { return this.CorDaPele; }
+    public String getCorDosOlhos()   { return this.corDosOlhos; }
+    public String getAnoNascimento() { return this.anoNascimento; }
+    public String getGenero()        { return this.genero; }
+    public String getHomeWorld()     { return this.homeworld; }
+
+    //----------------------------setters----------------------------------//
+
+    public void setNome(String nome)                    {  this.nome = nome; }
+    public void setAltura(int altura)                   {  this.altura = altura; }
+    public void setPeso(double peso)                    {  this.peso = peso; }
+    public void setCorDoCabelo(String corDoCabelo)      {  this.corDoCabelo = corDoCabelo; }
+    public void setCorDaPele(String corDaPele)          {  this.CorDaPele = corDaPele; }
+    public void setCorDosOlhos(String corDosOlhos)      {  this.corDosOlhos = corDosOlhos; }
+    public void setAnoNascimento(String anoNascimentos) {  this.anoNascimento = anoNascimentos; }
+    public void setGenero(String genero)                {  this.genero = genero; }
+    public void setHomeWorld(String homeworld)          {  this.homeworld = homeworld; }
+
+    //------------------------------clone--------------------------------//
+
+    public Personagem clone() {
+        Personagem clone = new Personagem();
+
+        clone.setAltura        (this.getAltura());
+        clone.setAnoNascimento (this.getAnoNascimento()); 
+        clone.setCorDaPele     (this.getCorDaPele());
+        clone.setCorDoCabelo   (this.getCorDoCabelo());
+        clone.setCorDosOlhos   (this.getCorDosOlhos());
+        clone.setGenero        (this.getGenero());
+        clone.setHomeWorld     (this.getHomeWorld());
+        clone.setNome          (this.getNome());
+        clone.setPeso          (this.getPeso());
+
+        return clone;
+    }
+
+    //------------------------------toString--------------------------------//
+
+    public String toString() {
+        return " ## " + this.getNome() + " ## " + this.getAltura() + " ## " + this.getPeso() + " ## " + this.getCorDoCabelo()  +
+         " ## " + this.getCorDaPele() + " ## " + this.getCorDosOlhos() + " ## " + this.getAnoNascimento() + " ## " +
+         this.getGenero() + " ## " + this.getHomeWorld() + " ## ";
+    }
+
+    //compare
+    public int compare(Personagem pers2) {
+        int resp = -1;
+        if (pers2 != null) resp = this.getNome().compareTo(pers2.getNome());
+        return resp;
+    }
+
+} 
+//----------------------------GerenciadorDeArquivo-----------------------------//
+
+class GerenciadorDeArquivo 
+{
+    //------------------------------atributos-----------------------------------//
+
+    public String caminhoArquivo;
+    public String conteudoArquivo;
+
+    //------------------------------lerArquivo---------------------------------//
+    
+    public void lerArquivo() {
+        Arq.openRead(this.caminhoArquivo);
+        this.conteudoArquivo = Arq.readLine();
+        Arq.close();
+    }
+
+    //---------------------------Separar-valor-do-Nome------------------------//
+    public String separarNome() {
+        int indiceDoAtributo = this.conteudoArquivo.indexOf("'name'") + 9;
+        String name = "";
+        while (this.conteudoArquivo.charAt(indiceDoAtributo) != 39) {
+            name += conteudoArquivo.charAt(indiceDoAtributo);
+            indiceDoAtributo++;
+        }
+        
+        return name;
+    }
+
+    //-----------------------------Separar-Altura----------------------------//
+
+    public String separarAltura() {
+        int indiceDoAtributo = this.conteudoArquivo.indexOf("'height'") + 11;
+        String altura = "";
+        while (this.conteudoArquivo.charAt(indiceDoAtributo) != 39) {
+            altura += conteudoArquivo.charAt(indiceDoAtributo);
+            indiceDoAtributo++;
+        }
+        if(altura.equals("unknown")) altura = "0";
+        return altura;
+    }
+
+    public int separarPeso() {
+        int indiceDoAtributo = this.conteudoArquivo.indexOf("'mass'") + 9;
+        String peso = "";
+        while (this.conteudoArquivo.charAt(indiceDoAtributo) != 39) {
+            if (conteudoArquivo.charAt(indiceDoAtributo) == ',') {
+                peso += '.'; 
+                indiceDoAtributo++;
+                continue;
+            }
+            
+            peso += conteudoArquivo.charAt(indiceDoAtributo);
+            indiceDoAtributo++;
+        }
+        if (peso.equals("unknown")) {
+            return 0;
+        } else {
+            double pesoDouble = Double.parseDouble(peso);
+            if (Math.floor(pesoDouble) == pesoDouble) {
+                return (int) pesoDouble;
+            } else {
+                return (int) Math.round(pesoDouble);
+            }
+        }
+    }
+
+    //---------------------------Separar-Cor-Do-Cabelo----------------------//
+
+    public String separarCorDoCabelo() {
+        int indiceDoAtributo = this.conteudoArquivo.indexOf("'hair_color'") + 15;
+        String corDoCabelo = "";
+        while (this.conteudoArquivo.charAt(indiceDoAtributo) != 39) {
+            corDoCabelo += conteudoArquivo.charAt(indiceDoAtributo);
+            indiceDoAtributo++;
+        }
+        return corDoCabelo;
+    }
+
+    //-----------------------separar-cor-da-pele-------------------------//
+
+    public String separarCorDaPele() {
+        int indiceDoAtributo = this.conteudoArquivo.indexOf("'skin_color'") + 15;
+        String corDaPele = "";
+        while (this.conteudoArquivo.charAt(indiceDoAtributo) != 39) {
+            corDaPele += conteudoArquivo.charAt(indiceDoAtributo);
+            indiceDoAtributo++;
+        }
+        return corDaPele;
+    }
+
+    //-----------------------separar-cor-dos-olhos-------------------//
+
+    public String separarCorDosOlhos() {
+        int indiceDoAtributo = this.conteudoArquivo.indexOf("'eye_color'") + 14;
+        String corDosOlhos = "";
+        while (this.conteudoArquivo.charAt(indiceDoAtributo) != 39) {
+            corDosOlhos += conteudoArquivo.charAt(indiceDoAtributo);
+            indiceDoAtributo++;
+        }
+        return corDosOlhos;
+    }
+
+    //-----------------------data-de-nascimento---------------------//
+
+    public String separarDataNascimento() {
+        int indiceDoAtributo = this.conteudoArquivo.indexOf("'birth_year'") + 15;
+        String dataNascimento = "";
+        while (this.conteudoArquivo.charAt(indiceDoAtributo) != 39) {
+            dataNascimento += conteudoArquivo.charAt(indiceDoAtributo);
+            indiceDoAtributo++;
+        }
+        return dataNascimento;
+    }
+
+    //------------------------separar-genero-----------------------//
+    
+    public String separarGenero() {
+        int indiceDoAtributo = this.conteudoArquivo.indexOf("'gender'") + 11;
+        String genero = "";
+        while (this.conteudoArquivo.charAt(indiceDoAtributo) != 39) {
+            genero += conteudoArquivo.charAt(indiceDoAtributo);
+            indiceDoAtributo++;
+        }
+        return genero;
+    }
+
+    //----------------------separar-homeworld---------------------//
+
+    public String separarHomeworld() {
+        int indiceDoAtributo = this.conteudoArquivo.indexOf("'homeworld'") + 14;
+        String homeworld = "";
+        while (this.conteudoArquivo.charAt(indiceDoAtributo) != 39) {
+            homeworld += conteudoArquivo.charAt(indiceDoAtributo);
+            indiceDoAtributo++;
+        }
+        return homeworld;
+    }
+
+    public void setAtributosPersonagem(Personagem pers) {
+        pers.setNome           ( this.separarNome() );
+        pers.setAltura         ( Integer.parseInt( this.separarAltura() ));
+        pers.setPeso           (this.getPeso());
+        pers.setCorDaPele      ( this.separarCorDaPele() );
+        pers.setCorDoCabelo    ( this.separarCorDoCabelo() );
+        pers.setCorDosOlhos    ( this.separarCorDosOlhos() );
+        pers.setAnoNascimento  ( this.separarDataNascimento() );
+        pers.setGenero         ( this.separarGenero() );
+        pers.setHomeWorld      ( this.separarHomeworld() );      
+            
+    }
+
+    //------------------------------setters-------------------------------//
+    
+    public void setCaminhoArquivo(String caminho) {
+        this.caminhoArquivo = caminho;
+    }
+
+    //------------------------------getters-----------------------------------//
+
+    public String getNome() { 
+        String nome = this.separarNome();
+        return nome; 
+    }
+    public int getAltura() { 
+        int altura = Integer.parseInt( this.separarAltura() );         
+        return altura; 
+    }
+    public double getPeso() { 
+        // double peso = Double.parseDouble(this.separarPeso() ); 
+        double peso = this.separarPeso();
+        return peso; 
+    }
+    public String getCorDoCabelo() { 
+        String corDoCabelo = this.separarCorDoCabelo();    
+        return corDoCabelo;
+    }
+    public String getCorDaPele() { 
+        String corDaPele = this.separarCorDaPele();    
+        return corDaPele; 
+    }
+    public String getCorDosOlhos() { 
+        String corDosOlhos = this.separarCorDosOlhos();
+        return corDosOlhos;
+    }
+    public String getAnoNascimento() { 
+        String anoNascimento = this.separarDataNascimento();
+        return anoNascimento; 
+    }
+    public String getGenero() { 
+        String genero = this.separarGenero();          
+        return genero; 
+    }
+    public String getHomeWorld() { 
+        String homeworld = this.separarHomeworld();    
+        return homeworld; 
+    }
+}
+
+class Main 
+{
+    //--------------------------------isFim--------------------------------//
+    
+    public static boolean isFim(String str) { return (str.length() == 3 && str.charAt(0) == 'F' && str.charAt(1) == 'I' && str.charAt(2) == 'M'); }
+
+    //--------------------------------main----------------------------------//
+    public static void main(String[] args) throws Exception {
+        MyIO.setCharset("utf-8");
+        
+        //objetos
+        BinaryTree binTreePers = new BinaryTree();
+        GerenciadorDeArquivo gerenciador = new GerenciadorDeArquivo(); 
+
+        //leitura de objetos (personagen)
+        do { 
+            //ler caminho do arquivo
+            gerenciador.caminhoArquivo = MyIO.readLine();
+            if(isFim(gerenciador.caminhoArquivo)) break;
+            
+            // cria-se referencia ao novo personagem
+            Personagem pers = new Personagem();
+            
+            //gerenciador ler valores do arquivo
+            gerenciador.lerArquivo();
+            
+            //set atributos do personagem
+            gerenciador.setAtributosPersonagem(pers);
+
+            //personagem é inserido na arvore
+            binTreePers.insert(pers); 
+
+        } while (true);
+
+        //persquina na arvore
+        do { 
+            //ler nome de personagem a ser procurado
+            String nomePers = MyIO.readLine();
+            if(isFim(nomePers)) break;
+            
+            MyIO.print(nomePers + ' ');
+            MyIO.println(binTreePers.search(nomePers)? "SIM":"NãO");
+
+        } while (true);
+    }
+}
